09dcd8adc306ac91d33a2ebd0c466ead3aec0edd
brajesh <brajeshjha108@gmail.com>
2022-12-26 18:57:16 +0530

    Completed: Integration of thimble insurance, testing needed though

---
+++ database/migrations/2022_12_26_125741_add_insurance_and_badges_columns_in_user_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_details', function (Blueprint $table) {
+            $table->boolean('is_insured')->default(0);
+            $table->boolean('provide_organic_service')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_details', function (Blueprint $table) {
+            $table->dropColumn([
+                'is_insured',
+                'provide_organic_service'
+            ]);
+        });
+    }
+};
