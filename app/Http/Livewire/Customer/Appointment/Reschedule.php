81f850e040b7c74d0937282d2e8551b06596c462
aman <itxamandeepkaur@gmail.com>
2023-01-04 15:34:58 +0530

    checkout page working properly

---
+++ app/Http/Livewire/Customer/Appointment/Reschedule.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Customer\Appointment;
+
+use Livewire\Component;
+
+class Reschedule extends Component
+{
+    public function render()
+    {
+        return view('livewire.customer.appointment.reschedule');
+    }
+}
---
+++ resources/views/livewire/customer/appointment/reschedule.blade.php
@@ -0,0 +1,4 @@
+<div>
+    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <h2>Reschedule page</h2>
+</div>
