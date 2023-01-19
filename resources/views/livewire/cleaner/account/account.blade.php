646fc2fe8b5ee99869646a10b781ff869a188bab
aman <itxamandeepkaur@gmail.com>
2023-01-19 16:49:05 +0530

    team page issues done

--- resources/views/livewire/cleaner/account/account.blade.php
+++ resources/views/livewire/cleaner/account/account.blade.php
@@ -46,6 +46,9 @@
                         <h6 class="title-label">Phone:</h6>
                         @if (@$fieldStatus == true && $action == 'contact_number')
                             <input type="number" value="{{ $user->contact_number }}" wire:model="contact_number" />
+                                <span style="color:red"> @error('contact_number')
+                                    {{ $message }}
+                                @enderror </span>
                             <span class="edit"><a class="link-design-2" wire:click="updateData('contact_number')"><i
                                         class="fas fa-save"></i></a></span>
                             <span class="cancel"><a href="javascript::void(0)" class="link-design-2"
