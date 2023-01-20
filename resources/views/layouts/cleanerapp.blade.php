d53d7f720ebb8a4db90b651afcb949ca0673d95a
Jashan Kapoor <itxjashankapoor@gmail.com>
2023-01-20 15:21:26 +0530

    Fixed: passed time is being displayed while booking cleaner

--- resources/views/layouts/cleanerapp.blade.php
+++ resources/views/layouts/cleanerapp.blade.php
@@ -8,15 +8,16 @@
         <meta name="site-url" content="{{ url('/') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
-    {{-- <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet"> --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
+
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/admin/css/admin.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/slick.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/croppie.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/slick-theme.css') }}" rel="stylesheet">
+
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
