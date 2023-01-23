320abafecc3a7f5971dfe389b7703e4d32838f52
Jashan Kapoor <itxjashankapoor@gmail.com>
2023-01-22 22:45:13 -0600

    Fixed: payment_failed status issue in admin side, header dropdown not working issue

--- resources/views/layouts/includes/script.blade.php
+++ resources/views/layouts/includes/script.blade.php
@@ -4,9 +4,9 @@
 <script src="{{asset('assets/js/main.js')}}"></script>
 <script src="{{asset('assets/js/croppie.js')}}"></script>
 {{-- <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script> --}}
-{{--
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
---}}
+
 
 <script src="{{asset('assets/js/slick.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js"></script>
